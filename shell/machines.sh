fac36e586c0409d1d2358a9425a34bbe49d44c25
Andrew Nolan <anolan@lanl.gov>
2025-12-11 16:30:27 +0000

    Add ALCF polaris as supported machine

--- shell/machines.sh
+++ shell/machines.sh
@@ -14,12 +14,27 @@ chrysalis_config(){
     load_tmux_module
 }
 
+polaris_config(){
+    if hash xalt_syshost 2>/dev/null; then
+         module unload xalt
+    fi
+}
+
 # set machine specific configurations
 case $MACHINE in
+    "aurora")
+        aurora_config
+        ;;
     "chrysalis")
         chrysalis_config
+        ;;
+    "polaris")
+        polaris_config
         ;;
 esac
 
 # clean up namespace
+unset load_tmux_module
+unset aurora_config
 unset chrysalis_config
+unset polaris_config
